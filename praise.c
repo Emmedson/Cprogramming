0c92ef76b0ade3acfe5231905deab0fa5d58bcc4
Emmedson <livinusdogz@gmail.com>
2022-12-24 12:12:34 +0100

    a simple package c executable code

---
+++ praise.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/*this comment should tell the name and age of a student*/
+int main (void)
+{
+    int age = 12;
+    char name [] = "t";
+
+    
+
+    printf ("my name is %s", name);
+    printf (" i am %d old", age);
+
+    return (0);
+
+
+}
+
+
